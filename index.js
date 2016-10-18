d1ec9acfe09ff614e2cba05138bd56d17404f568
alex mwaleh <alex.mwaleh@andela.com>
2016-10-18 18:13:23 +0300

    Add Documentation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var seneca = require ('seneca')();
-seneca.use('./process.js');
-seneca.act({role:'process', cmd: 'sum', numbers: [1,2,3]}, function(err, result){
 
+// define the file that will be used
+seneca.use('./process.js');
+
+/* Call the function you want to use in the process.js file
+JSON object is sent with role and cmd along with the arguments
+`numbers`
+
+*/
+seneca.act({role:'process', cmd: 'sum', numbers: [1,2,3]}, function(err, result){
   console.log(result);
 })
--- process.js
+++ process.js
@@ -1,7 +1,10 @@
 module.exports = function (options){
 
    var seneca = this;
+   // Define function add and add sum to it
    seneca.add({role:'process', cmd:'sum'}, sum);
+
+// declare the sum function
    function sum (args, done){
      var numbers = args.numbers;
      var result = numbers.reduce( function(a,b){
